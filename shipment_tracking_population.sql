8a17cef080c9c0fe2a80a808132d68ee6071decb
andreizota29 <andrei.zota174@gmail.com>
2026-03-15 17:02:24 +0200

    1

--- shipment_tracking_population.sql
+++ shipment_tracking_population.sql
@@ -3,13 +3,13 @@
 
 DECLARE
     v_batch_size CONSTANT NUMBER := 50000;
-    v_total_rows CONSTANT NUMBER := 100000000; 
+    v_total_rows CONSTANT NUMBER := 100 000 000; 
 BEGIN
     FOR i IN 1 .. (v_total_rows / v_batch_size) LOOP
         INSERT /*+ APPEND */ INTO Shipment_Tracking (track_id, ship_id, loc_timestamp, lat_coord, long_coord)
         SELECT 
             (i-1)*v_batch_size + level,
-            trunc(dbms_random.value(1, 1000000)),        -- Linking to 1M shipments
+            trunc(dbms_random.value(1, 1 000 000)),        -- Linking to 1M shipments
             sysdate - dbms_random.value(0, 365),         -- Random date within last year
             44.4268 + dbms_random.value(-1, 1),          -- Realistic Latitude (around Romania/EU)
             26.1025 + dbms_random.value(-1, 1)           -- Realistic Longitude

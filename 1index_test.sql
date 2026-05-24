814baee6a5e7548a4944104def06f5b56bdfebeb
Zota Andrei <andrei.zota174@gmail.com>
2026-05-24 22:33:51 +0300

    Update 1index_test.sql

--- 1index_test.sql
+++ 1index_test.sql
@@ -1,3 +1,49 @@
+SET LINESIZE 200;
+SET PAGESIZE 50;
+
+
+-- idx_track_id TEST
+-- ignore index
+SET TIMING ON
+SELECT /*+ FULL(st) GATHER_PLAN_STATISTICS */ * FROM LOG_ARCHITECT.Shipment_Tracking st 
+WHERE track_id = 50000000;
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY_CURSOR(FORMAT=>'ALLSTATS LAST'));
+--with index
+SELECT /*+ INDEX(st idx_track_id) GATHER_PLAN_STATISTICS */ * FROM LOG_ARCHITECT.Shipment_Tracking st 
+WHERE track_id = 50000000;
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY_CURSOR(FORMAT=>'ALLSTATS LAST'));
+
+
+-- idx_cust_status
+-- ignore index
+ALTER INDEX LOG_ARCHITECT.idx_cust_status INVISIBLE;
+SET TIMING ON
+SELECT /*+ GATHER_PLAN_STATISTICS */ s.ship_id, c.cust_name, s.status
+FROM LOG_ARCHITECT.Customers c
+JOIN LOG_ARCHITECT.Shipments s ON c.id = s.cust_id
+WHERE s.cust_id = 1 AND s.status = 'In Transit';
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY_CURSOR(FORMAT=>'ALLSTATS LAST'));
+-- with index
+ALTER INDEX LOG_ARCHITECT.idx_cust_status VISIBLE;
+SELECT /*+ GATHER_PLAN_STATISTICS */ s.ship_id, c.cust_name, s.status
+FROM LOG_ARCHITECT.Customers c
+JOIN LOG_ARCHITECT.Shipments s ON c.id = s.cust_id
+WHERE s.cust_id = 1 AND s.status = 'In Transit';
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY_CURSOR(FORMAT=>'ALLSTATS LAST'));
+
+-- idx_func_time
+-- ignore index
+SELECT /*+ NO_INDEX(st idx_func_time) GATHER_PLAN_STATISTICS */ COUNT(*) 
+FROM LOG_ARCHITECT.Shipment_Tracking st 
+WHERE TRUNC(loc_timestamp) = TRUNC(SYSDATE - 10);
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY_CURSOR(FORMAT=>'ALLSTATS LAST'));
+-- with index
+SELECT /*+ INDEX(st idx_func_time) GATHER_PLAN_STATISTICS */ COUNT(*) 
+FROM LOG_ARCHITECT.Shipment_Tracking st 
+WHERE TRUNC(loc_timestamp) = TRUNC(SYSDATE - 10);
+SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY_CURSOR(FORMAT=>'ALLSTATS LAST'));
+
+
 
 --no index
 SELECT /*+ FULL(st) */ count(*) 
